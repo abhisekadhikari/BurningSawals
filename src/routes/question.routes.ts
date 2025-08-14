78b90b12b87c5408cc8f2fb77893f614f0400e7e
abhisekadhikari <abhisekadhikari1906@gmail.com>
2025-08-14 09:50:23 +0530

    Implement Zod validation middleware and question schema validation

--- src/routes/question.routes.ts
+++ src/routes/question.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import questionSchemaValidator from "../validators/question.validator";
+import { validator } from "../middlewares/validator.middleware";
+import { createQuestion } from "../controllers/questions.controller";
 
 const questionRouer = Router();
+
+questionRouer
+    .route("/")
+    .post(validator(questionSchemaValidator.upsertQuestion), createQuestion);
 
 export default questionRouer;
